627b08cc5c99d6688055846f63c4b29e2bb0cc54
Geethu M A <geethuvinil30@gmail.com>
2023-05-31 16:11:05 +0400

    homepage_firestore_onprogress

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,6 +14,16 @@ class _HomePageState extends State<HomePage> {
       TextEditingController();
   TextEditingController _textEditingTaskDescriptionController =
       TextEditingController();
+      final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+      final FirebaseAuth _auth = FirebaseAuth.instance;
+      late  CollectionReference todoRef;
+
+      @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    todoRef = _firestore.collection('todoTask');
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,39 +58,62 @@ class _HomePageState extends State<HomePage> {
                 decoration: InputDecoration(hintText: "Description"),
               ),
             ),
-            TextButton(onPressed: () {}, child: Text("Add more tasks")),
+            TextButton(onPressed: () async{
+                await todoRef.add({
+                  "title":_textEditingTaskNameController.text,
+                  "description":_textEditingTaskDescriptionController.text,
+                  "userId":_auth.currentUser!.uid,
+                });
+                _textEditingTaskNameController.clear();
+                _textEditingTaskDescriptionController.clear();
+            }, child: Text("Add")),
             Divider(),
-        //  Expanded(child: StreamBuilder(builder: (BuildContext context, snapshot) {
-        //    return ListView.builder(itemBuilder: (context, index) {
-             
-        //    },);
-        //  },),),
+
+             Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: todoRef.where("userId",isEqualTo: _auth.currentUser!.uid).snapshots(),
+                builder: (BuildContext context,AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if(!snapshot.hasData){
+                  return  Center(child: CircularProgressIndicator());
+                  }
+                  final List<DocumentSnapshot> documents = snapshot.data!.docs;
+               return ListView.builder(
+                itemCount: documents.length,
+                itemBuilder:  (BuildContext context,int index) {
+                final document = documents[index];
+                 return ListTile(
+             title: Text(document["title"]),
+                 );
+               },);
+             },),),
           ],
         ),
       ),
     );
   }
 
- Padding  taskTile() {
-    return    Padding(
+  Padding taskTile() {
+    return Padding(
       padding: const EdgeInsets.all(8.0),
       child: ListView.builder(
-                shrinkWrap: true,
-                itemBuilder: (context, index) {
-                  return  Row(
-                    children: [
-                      Column(children: [
-                        Text("task1"),
-                        Text("Aadasd")
-                      ],),
-                      IconButton(onPressed: (){}, icon: Icon(Icons.edit,color: Colors.blue,)),
-                                            IconButton(onPressed: (){}, icon: Icon(Icons.delete)),
-
-                    ],
-                  );
-                },
-               
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          return Row(
+            children: [
+              Column(
+                children: [Text("task1"), Text("Aadasd")],
               ),
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.edit,
+                    color: Colors.blue,
+                  )),
+              IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+            ],
+          );
+        },
+      ),
     );
   }
 }
--- lib/login Details/view/login_details_page.dart
+++ lib/login Details/view/login_details_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:todoo/home/view/home_page.dart';
 
@@ -11,6 +12,7 @@ class LoginDetailsPage extends StatefulWidget {
 class _LoginDetailsPageState extends State<LoginDetailsPage> {
   TextEditingController _textEditingEmailIdController = TextEditingController();
   TextEditingController _textEditingPasswordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,40 +21,62 @@ class _LoginDetailsPageState extends State<LoginDetailsPage> {
         leading: BackButton(),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-              SizedBox(
-              height: 70,
-            ),
-            Center(
-              child: Text("Enter your email and password to Login",
-              style: TextStyle(
-                 color: Colors.purple.shade600,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18
-              ),),
-            ),
-loginCredentials(text: "Enter your email id",controller: _textEditingEmailIdController,type: TextInputType.emailAddress),
-loginCredentials(text: "Enter your password",controller: _textEditingPasswordController,type: TextInputType.visiblePassword),
-        
-        SizedBox(
-          height: 30,
-        ),
-        TextButton(onPressed: (){}, child: Text("Forgot password?")),
-        ElevatedButton(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+                SizedBox(
+                height: 70,
+              ),
+              Center(
+                child: Text("Enter your email and password to Login",
+                style: TextStyle(
+                   color: Colors.purple.shade600,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18
+                ),),
+              ),
+        loginCredentials(text: "Enter your email id",controller: _textEditingEmailIdController,type: TextInputType.emailAddress,
+        validator: (value) {
+          if(value!.isEmpty){
+            return "Please  fill this field";
+          }
+        },),
+        loginCredentials(text: "Enter your password",controller: _textEditingPasswordController,type: TextInputType.visiblePassword,
+        validator: (value) {
+          if(value!.isEmpty){
+            return "Please  fill this field";
+          }
+        },),
           
-          onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(),));
-          }, child: Text("Login"))
-          ],
+          SizedBox(
+            height: 30,
+          ),
+          TextButton(onPressed: (){}, child: Text("Forgot password?")),
+          ElevatedButton(
+            
+            onPressed: () async{
+            try {
+              final _auth = FirebaseAuth.instance;
+              final userReference = await _auth.signInWithEmailAndPassword(email: _textEditingEmailIdController.text,
+               password: _textEditingPasswordController.text);
+                Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(),));
+            } catch (e) {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Invalid email or password")));
+            }
+             
+            }, child: Text("Login"))
+            ],
+          ),
         ),
       ),
     );
   }
- Padding loginCredentials({String? text, TextEditingController? controller,TextInputType? type}){
+ Padding loginCredentials({String? text, TextEditingController? controller,TextInputType? type,String? Function(String?)? validator}){
     return Padding(
       padding: const EdgeInsets.only(top: 30,left: 18,right: 18),
-      child: TextField(
+      child: TextFormField(
+        validator: validator,
     controller:controller ,
     decoration: InputDecoration(
       hintText: text
